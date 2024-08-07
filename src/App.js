344e112ced3a02ea63228d7925a5cedc6aace720
Annak506 <annakleinloog@hotmail.com>
2024-08-07 12:35:40 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
+
       Weather App
       <footer> This project was coded by Anna Bannenberg-Kleinloog and is {" "}</footer>
       <a href="https://github.com/Annak506/reactweek5weatherapp" 
       target="_blank"> open-sourced on Github</a>
+      </div>
     </div>
   );
 }
