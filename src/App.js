9f824874cd121957f712daee1d98fbcafa7f476c
Annak506 <annakleinloog@hotmail.com>
2024-08-07 11:05:39 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       Weather App
+      <footer> This project was coded by Anna Bannenberg-Kleinloog and is {" "}</footer>
+      <a href="https://github.com/Annak506/reactweek5weatherapp" 
+      target="_blank"> open-sourced on Github</a>
     </div>
   );
 }
